bbd67c5f8a2795dd02b84f1499b02e3326ff4747
d3db0t <ShadyTantawy@protonmail.com>
2017-05-31 00:10:58 +0200

    Added colors

--- Nullnetwork.py
+++ Nullnetwork.py
@@ -5,6 +5,10 @@ import time
 import sys
 
 class Nullnetwork:
+	
+	# Colors
+	BLUE, RED, WHITE, YELLOW, MAGENTA, GREEN, END = '\33[94m', '\033[91m',
+	'\33[97m', '\33[93m', '\033[1;35m', '\033[1;32m', '\033[0m'
 
 
 	def __init__(self):

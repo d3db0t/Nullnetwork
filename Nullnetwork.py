23e5fbdbbcc174cf0d4d944603a9c4fb407b0209
d3db0t <ShadyTantawy@protonmail.com>
2017-05-31 03:56:25 +0200

    Big update [1]
    -Added optionBanner
    -Added main method
    -Plus some updates

--- Nullnetwork.py
+++ Nullnetwork.py
@@ -32,11 +32,24 @@ class Nullnetwork:
 						.format(self.BLUE, self.YELLOW, self.END) + "\n")
 
 
+	def optionBanner(self):
+		print "\n{0}Choose an option:{1} \n".format(self.GREEN, self.END)
+		print "{0}[{1}1{2}] {3}Attack Broadcast{4}".format(self.YELLOW,
+		self.BLUE, self.YELLOW, self.WHITE, self.END) + "\n"
+		time.sleep(0.3)
+		print "{0}[{1}2{2}] {3}Attack Specific Device{4}".format(self.YELLOW,
+		self.BLUE, self.YELLOW, self.WHITE, self.END) + "\n"
+		time.sleep(0.3)
+		print "{0}[{1}E{2}] {3}Exit{4}".format(self.YELLOW,
+		self.BLUE, self.YELLOW, self.WHITE, self.END) + "\n"
+		time.sleep(0.3)
+
+
+
 	def getInterface(self):
 		str = commands.getoutput("ifconfig")
 		try:
 			i = re.search('(w).*(:)', str).group(0)[:-1]
-			#print i.group(0)[:-1]
 		except:
 			print "{0}No Match Found!{1}".format(self.RED, self.END)
 			i = raw_input("Network Interface: ")
@@ -47,19 +60,21 @@ class Nullnetwork:
 
 
 	def killProcess(self):
-		print "{0}Killing process...{1}".format(self.BLUE,self.END),
+		print "{0}[+] {1}Killing process...{2}".format(self.YELLOW,
+		self.BLUE, self.END),
 		try:
 			os.system("airmon-ng check kill")
 			print "{0}OK{1}".format(self.YELLOW, self.END)
+			time.sleep(1)
 		except:
 			print "{0}Unable to kill process :({1}".format(self.RED, self.END)
 
 
 	def startAirmon(self):
 		try:
-			if self.interface == "":
-				i = raw_input("Network Interface: ")
-				self.interface = i
+			#if self.interface == "":
+				#i = raw_input("Network Interface: ")
+				#self.interface = i
 			os.system("airmon-ng start " + self.interface)
 		except:
 			print "{0}Airmon failed to start :({1}".format(self.RED, self.END)
@@ -73,7 +88,8 @@ class Nullnetwork:
 
 
 	def getBSSID(self):
-		b = raw_input("BSSID: ")
+		m = "{0}BSSID:{1} ".format(self.GREEN, self.END)
+		b = raw_input(m)
 		self.bssid = b
 
 
@@ -85,7 +101,8 @@ class Nullnetwork:
 
 
 	def getChannel(self):
-		c = raw_input("Channel: ")
+		ch = "{0}Channel:{1} ".format(self.GREEN, self.END)
+		c = raw_input(ch)
 		self.channel = c
 
 
@@ -97,16 +114,38 @@ class Nullnetwork:
 		except:
 			print "{0}Final Airodump failed :({1}".format(self.RED, self.END)
 #-------------------------------------------------------
-N = Nullnetwork()
-N.banner()
-N.getInterface()
-N.killProcess()
-N.startAirmon()
-N.getInterface()
-N.airodump_ng()
-N.getBSSID()
-N.getChannel()
-N.finalAirodump()
-N.aireplay_ng()
-#print N.interface
-#print N.monitor
+
+def main():
+	N.banner()
+	try:
+		while True:
+			N.optionBanner()
+			terminal = "{0}Nullnetwo{1}rk{2}> {3}".format(N.RED, N.BLUE, N.WHITE, N.END)
+			choice   = raw_input(terminal)
+			if choice == '1':
+				attackBroadCast()
+			elif choice == 'E':
+				raise KeyboardInterrupt
+			else:
+				os.system("clear")
+				print "{0}Please choose a valid option!{1}".format(N.RED, N.END)
+
+	except KeyboardInterrupt:
+		print "\n\n{0}Happy Hacking!{1}".format(N.YELLOW, N.END)
+
+
+def attackBroadCast():
+	N.getInterface()
+	N.killProcess()
+	N.startAirmon()
+	N.getInterface()
+	N.airodump_ng()
+	N.getBSSID()
+	N.getChannel()
+	N.finalAirodump()
+	N.aireplay_ng()
+
+
+if __name__ == '__main__':
+	N = Nullnetwork()
+	main()
